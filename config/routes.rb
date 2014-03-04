f95958417a4d1b80d883dbcd36f18f3d18df7b1d
Daniel <dzuazaga@gmail.com>
2014-03-04 15:09:45 -0300

    all files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Totem::Application.routes.draw do
   resources :totem_locations
 
+ get '/foo', :to => redirect('/lala.html')
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
